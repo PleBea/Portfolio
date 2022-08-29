18b8eec45e8996e6243ff68d7d8d9dde8e12d7b6
plebea <bini060315kim@gmail.com>
2022-08-29 14:50:06 +0900

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Index.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={}>
+      
+    </div>
+  )
+}
+
+export default Home
